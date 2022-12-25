a6a87a15d1c101933824c60eb5ff2643ebf241fe
Hambinn <ilhambintangn@gmail.com>
2022-12-25 19:49:57 +0700

    fix: add penjualan link to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,22 @@ function Navbar() {
     navigate("/login");
   };
 
+  const penjualan = () => {
+    navigate("/penjualan");
+  };
+
   return (
     <nav className="flex justify-end bg-primary h-16 items-center gap-10 pr-5">
       <a href="#" className="p-2 px-4 text-white font-bold text-xl">
         Dashboard
       </a>
-      <a href="#" className="p-2 px-4 text-white font-bold text-xl">
-        Penjualan
-      </a>
       <button
-        href="#"
+        className="p-2 px-4 text-white font-bold text-xl"
+        onClick={penjualan}
+      >
+        Penjualan
+      </button>
+      <button
         className="mr-6 p-2 px-4 text-white font-bold text-xl"
         onClick={logout}
       >
