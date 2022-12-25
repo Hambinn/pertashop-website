9113ba021cf2fb2351a82a41106c0fb2cd872612
Hambinn <ilhambintangn@gmail.com>
2022-12-25 18:37:37 +0700

    feat: add input penjualan baru button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-
+import { useNavigate } from "react-router";
+import { useCookies } from "react-cookie";
 function Navbar() {
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+  const logout = () => {
+    removeCookie("jwt");
+    navigate("/login");
+  };
+
   return (
     <nav className="flex justify-end bg-primary h-16 items-center gap-10 pr-5">
       <a href="#" className="p-2 px-4 text-white font-bold text-xl">
@@ -9,9 +17,13 @@ function Navbar() {
       <a href="#" className="p-2 px-4 text-white font-bold text-xl">
         Penjualan
       </a>
-      <a href="#" className="mr-6 p-2 px-4 text-white font-bold text-xl">
+      <button
+        href="#"
+        className="mr-6 p-2 px-4 text-white font-bold text-xl"
+        onClick={logout}
+      >
         Logout
-      </a>
+      </button>
     </nav>
   );
 }
--- src/pages/Penjualan.jsx
+++ src/pages/Penjualan.jsx
@@ -2,10 +2,32 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate, Navigate } from "react-router";
+import { useCookies } from "react-cookie";
+import { ToastContainer, toast } from "react-toastify";
 function Penjualan() {
   const [matrix, setMatrix] = useState(null);
-
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+  const navigate = useNavigate();
   useEffect(() => {
+    const vefifyUser = async () => {
+      if (!cookies.jwt) navigate("/login");
+      else {
+        const { data } = await axios.post(
+          "http://localhost:3000",
+          {},
+          { withCredentials: true }
+        );
+        if (!data.status) {
+          removeCookie("jwt");
+          navigate("/login");
+        } else {
+          toast.success(`Welcome ${data.user}`, { theme: "dark" });
+        }
+      }
+    };
+    vefifyUser();
+
     const getUserId = async () => {
       const { data } = await axios.post(
         "http://localhost:3000/penjualan",
@@ -25,44 +47,61 @@ function Penjualan() {
     return <p>Loading...</p>;
   }
 
+  const inputPenjualan = () => {
+    navigate("/inputpenjualan");
+  };
+
   return (
-    <div>
+    <>
       <Navbar />
-      <table className="table-auto mx-auto border my-24 rounded-xl">
-        <thead>
-          <tr>
-            <th className="px-4 py-2 border bg-secondary">Tanggal</th>
-            <th className="px-4 py-2 border bg-secondary">Meteran Awal</th>
-            <th className="px-4 py-2 border bg-secondary">Meteran Akhir</th>
-            <th className="px-4 py-2 border bg-secondary">
-              Penjualan Bukan Member
-            </th>
-            <th className="px-4 py-2 border bg-secondary">Penjualan Diantar</th>
-            <th className="px-4 py-2 border bg-secondary">Penjualan Member</th>
-            <th className="px-4 py-2 border bg-secondary">STIK</th>
-            <th className="px-4 py-2 border bg-secondary"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {matrix.map((item) => (
-            <tr key={item}>
-              <td className="px-4 py-2 border">{item[0]}</td>
-              <td className="px-4 py-2 border">{item[1]}</td>
-              <td className="px-4 py-2 border">{item[2]}</td>
-              <td className="px-4 py-2 border">{item[3]}</td>
-              <td className="px-4 py-2 border">{item[4]}</td>
-              <td className="px-4 py-2 border">{item[5]}</td>
-              <td className="px-4 py-2 border">{item[6]}</td>
-              <td className="px-4 py-2 border">
-                <button className="bg-secondary text-white px-4 py-2 rounded-lg">
-                  Edit
-                </button>
-              </td>
+      <div className="flex flex-col min-h-screen mx-20">
+        <button
+          className="bg-secondary text-white px-4 py-2 rounded-lg w-[20%] mt-20"
+          onClick={inputPenjualan}
+        >
+          Input Penjualan Baru
+        </button>
+        <table className="table-auto mx-auto border mt-16 rounded-xl w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 border bg-secondary">Tanggal</th>
+              <th className="px-4 py-2 border bg-secondary">Meteran Awal</th>
+              <th className="px-4 py-2 border bg-secondary">Meteran Akhir</th>
+              <th className="px-4 py-2 border bg-secondary">
+                Penjualan Bukan Member
+              </th>
+              <th className="px-4 py-2 border bg-secondary">
+                Penjualan Diantar
+              </th>
+              <th className="px-4 py-2 border bg-secondary">
+                Penjualan Member
+              </th>
+              <th className="px-4 py-2 border bg-secondary">STIK</th>
+              <th className="px-4 py-2 border bg-secondary"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {matrix.map((item) => (
+              <tr key={item}>
+                <td className="px-4 py-2 border">{item[0]}</td>
+                <td className="px-4 py-2 border">{item[1]}</td>
+                <td className="px-4 py-2 border">{item[2]}</td>
+                <td className="px-4 py-2 border">{item[3]}</td>
+                <td className="px-4 py-2 border">{item[4]}</td>
+                <td className="px-4 py-2 border">{item[5]}</td>
+                <td className="px-4 py-2 border">{item[6]}</td>
+                <td className="px-4 py-2 border  justify-center flex">
+                  <button className="bg-secondary text-white px-4 py-2 rounded-lg ">
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <ToastContainer />
+      </div>
+    </>
   );
 }
 
