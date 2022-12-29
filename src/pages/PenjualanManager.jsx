292cec862062e4145a5424b5d757fa33abe8f3f3
haidar_ihza <ihzaulhaqidar@gmail.com>
2022-12-29 16:26:05 +0700

    feat: add data penjualan for manager

--- src/pages/PenjualanManager.jsx
+++ src/pages/PenjualanManager.jsx
@@ -7,54 +7,129 @@ import { useCookies } from "react-cookie";
 import { ToastContainer, toast } from "react-toastify";
 
 function PenjualanManager() {
-  const [matrix, setMatrix] = useState(null);
-  const [cookies, setCookie, removeCookie] = useCookies([]);
-  const navigate = useNavigate();
-  useEffect(() => {
-    const verifyUser = async () => {
-      if (!cookies.jwt) navigate("/login");
-      else {
-        const { data } = await axios.post(
-          "http://localhost:3000",
-          {},
-          { withCredentials: true }
-        );
-        if (!data.status) {
-          removeCookie("jwt");
-          navigate("/login");
-        } else {
-          toast.success(`Welcome ${data.user}`, { theme: "dark" });
-        }
-      }
-    };
-    verifyUser();
-
-    const fetchData = async () => {
-        const response = await axios.get("http://localhost:3000/penjualanmanager");
-        response.data.values.shift();
-        setMatrix(response.data.values);
-    };
-    fetchData();
-    }, []);
-    
-    if (!matrix) {
-        return <p>Loading...</p>;
+    const [matrix, setMatrix] = useState([]);
+    const [cookies, setCookie, removeCookie] = useCookies([]);
+    const navigate = useNavigate();
+    let curr_month;
+    switch (new Date().getMonth()) {
+    case 0:
+        curr_month = "Januari";
+        break;
+    case 1:
+        curr_month = "Februari";
+        break;
+    case 2:
+        curr_month = "Maret";
+        break;
+    case 3:
+        curr_month = "April";
+        break;
+    case 4:
+        curr_month = "Mei";
+        break;
+    case 5:
+        curr_month = "Juni";
+        break;
+    case 6:
+        curr_month = "Juli";
+        break;
+    case 7:
+        curr_month = "Agustus";
+        break;
+    case 8:
+        curr_month = "September";
+        break;
+    case 9:
+        curr_month = "Oktober";
+        break;
+    case 10:
+        curr_month = "November";
+        break;
+    case 11:
+        curr_month = "Desember";
+        break;
     }
+    const [month, setMonth] = useState("");
+    const [listMonth, setListMonth] = useState([]);
+    useEffect(() => {
+        const verifyUser = async () => {
+        if (!cookies.jwt) navigate("/login");
+        else {
+            const { data } = await axios.post(
+            "http://localhost:3000",
+            {},
+            { withCredentials: true }
+            );
+            if (!data.status) {
+            removeCookie("jwt");
+            navigate("/login");
+            } else {
+            toast.success(`Welcome ${data.user}`, { theme: "dark" });
+            }
+        }
+        };
+        verifyUser();
 
-  const inputPenjualan = () => {
-    navigate("/inputpenjualanmanager");
-  };
+        // const data = {
+        //     month: month,
+        // }
+        // const fetchData = async () => {
+        //     const response = await axios.post("http://localhost:3000/penjualanmanager", data);
+        //     setMatrix(response.data.values);
+        // };
+        // fetchData();
 
-  return (
+        const getMonth = async () => {
+            const response = await axios.get("http://localhost:3000/sheetslist");
+            setListMonth(response.data);
+        };
+        getMonth();
+        }, []);
+        
+        if (!matrix) {
+            return <p>Loading...</p>;
+        }
+
+        function handleChange(e) {
+            const fetchData = async () => {
+                const response = await axios.post("http://localhost:3000/penjualanmanager", {
+                    month: e.target.value,
+                });
+                setMatrix(response.data.values);
+            };
+            fetchData();
+            console.log("Sebelum: " + month)
+            setMonth(e.target.value);
+            console.log("Sesudah: " + data.month);
+            console.log(matrix);
+        }
+
+    return (
     <>
       <Navbar />
-      <div className="flex flex-col min-h-screen mx-20">
-        <button
-          className="bg-secondary text-white px-4 py-2 rounded-lg w-[20%] mt-20"
-          onClick={inputPenjualan}
-        >
-          Input Penjualan Baru
-        </button>
+      <div className="flex flex-col min-h-screen mx-10">
+      <form action="" className="flex flex-col gap-y-2 ">
+            <label className="font-semibold text-base">Penjualan Harian Bulan: </label>
+            <select              
+              name="month"
+              id="month"
+            //   value={month}
+              className="border-3 py-3 px-2  text-black border-secondary focus:border-primary focus:border-3 rounded w-1/2"
+              onChange={handleChange}>
+                    <option value="">--- Pilih Bulan ---</option>
+                {listMonth.map((item) => (
+                    <option value={item}>{item}</option>
+                ))}
+            </select >
+        </form>
+        <p className="text-center text-black">
+        <br />
+            PENJUALAN HARIAN PERTAMAX
+        <br />
+            PERTASHOP 5P.661.35
+        <br />
+            Bulan {month}
+        </p>
         <table className="table-auto mx-auto border mt-16 rounded-xl w-full">
           <thead>
             <tr>
